565804593eb57b5288e997d7ebc588a9afc63ef8
sauravtom <stomatrix@gmail.com>
2016-08-06 15:05:14 +0530

    foo

--- fb_chatbot/urls.py
+++ fb_chatbot/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import patterns,url
 from fb_chatbot import views
+from fb_chatbot import MyQuoteBotView
 
 urlpatterns = patterns('',
 	url(r'^$', views.index,name = 'index'),
 	url(r'^hi/$', views.hello,name = 'hello'),
-	url(r'^hello/hi/$', views.hello,name = 'hello'))
+	url(r'^hello/hi/$', views.hello,name = 'hello'),
+	url(r'^facebook_auth/$', MyQuoteBotView.as_view()))
